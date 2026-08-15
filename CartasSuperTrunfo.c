61061b392f14b8914d8e38a889047f389fe7d4c2
eldergomes86 <202605172217@alunos.estacio.br>
2026-08-15 20:36:41 +0000

    Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,28 +4,31 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
-  char estado[1];
-  char codigo[3];
-  char cidade[50];
-  int populacao;
-  float area;
-  float pib;
-  int pontos;
+// Área para definição das variáveis para armazenar as propriedades das cidades
+char estado[1];
+char codigo[3];
+char cidade[50];
+float populacao;
+float area;
+float pib;
+int pontos;
+float dp;
+float ppc;
 
+void entradaDados() {
+ 
   // Área para entrada de dados
   printf("Digite o Estado: \n");
-  scanf("%1s", &estado);
+  scanf("%s", &estado);
 
   printf("Digite o código da carta (Estado + número): \n");
-  scanf("%2s", &codigo);
+  scanf("%s", &codigo);
 
   printf("Digite o nome da cidade: \n");
-  scanf("%49s", &cidade);
+  scanf("%s", &cidade);
 
   printf("Digite a população: \n");
-  scanf("%d", &populacao);
+  scanf("%f", &populacao);
 
   printf("Digite a área: \n");
   scanf("%f", &area);
@@ -36,14 +39,35 @@ int main() {
   printf("Digite o número de pontos turísticos: \n");
   scanf("%d", &pontos);
 
-  // Área para exibição dos dados da cidade
+} 
+
+float calculo(){
+  
+  dp = populacao / area;
+  ppc = pib / populacao;
+
+}
+
+int resultado(){
+
+ // Área para exibição dos dados da cidade
   printf("Estado: %s\n", estado);
   printf("Código da carta: %s\n", codigo);
   printf("Nome da cidade: %s\n", cidade);
-  printf("População: %d\n", populacao);
+  printf("População: %f\n", populacao);
   printf("Área: %.2f km²\n", area);
+  printf("Densidade Populacional: %.2f\n", dp);
   printf("PIB: %.2f bilhões de reais\n", pib);
+  printf("PIB Per Capita: %.2f\n", ppc);
   printf("Número de pontos turísticos: %d\n", pontos);
 
-return 0;
-} 
+}
+
+int main(){
+
+  entradaDados();
+  calculo();
+  resultado();
+  return 0;
+
+}
